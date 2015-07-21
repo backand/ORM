a165720ba8dd7001bc66168c88831175558dceb0
Yoram Kornatzky <kornatzky@gmail.com>
2015-07-21 10:00:04 +0300

    Add Tests for Validator

--- test/unit_functional_test.js
+++ test/unit_functional_test.js
@@ -26,4 +26,145 @@ describe("validate", function(){
   		);
 		done();
 	});
+
+	it("schema with relationships cannot require relationship columns, n:m relationships not allowed", function(done){
+		var v = validator(
+			[ 	
+				{
+					"name": "user",
+					"fields": {
+						"name": {
+							"type": "string"
+						},
+						"age": {
+							"type": "datetime",
+						},
+						"dogs":{
+							"collection": "pet",
+							"via": "owner",
+							"required": true
+						}
+					}
+				},
+
+				{ 
+
+					"name": "pet",
+
+					"fields": {
+						"name": {
+							"type": "string"
+						},
+						"registered": {
+							"type": "boolean"
+						},
+						"owner":{
+							"object": "user",
+							"required": true
+						}
+					}
+						
+				},
+
+				{
+					"name": "walker",
+					"fields": {
+						"name": {
+							"type": "string"
+						},
+						"age": {
+							"type": "datetime"
+						},
+						"dogs":{
+							"collection": "animal",
+							"via": "owners"
+						}
+					}
+				},
+
+
+				{
+					"name": "animal",
+					"fields": {
+						"name": {
+							"type": "string"
+						},
+						"breed": {
+							"type": "string"
+						},
+						"owners":{
+							"collection": "walker",
+							"via": "dogs"
+						}
+					}
+				}
+			]
+		);
+		expect(v).to.deep.equal(
+			{ 
+				valid: false,
+  			    warnings: [
+  			    	"a relationship column cannot be required to be set or not set:user dogs",
+      				"a relationship column cannot be required to be set or not set:pet owner",
+      			    "multi select relationship are not allowed: relation walker attribute dogs and relation animal attribute owners",
+      			    "multi select relationship are not allowed: relation animal attribute owners and relation walker attribute dogs"
+  			    ] 
+  			}
+  		);
+		done();
+	});
+
+	it("declare valid schema with relationships", function(done){
+		var v = validator(
+			[ 	
+							
+				{
+					"name": "R",
+
+					"fields": {
+						"A": {
+							"type": "float",
+							"defaultValue": 20
+						},
+
+						"B": {
+							"type": "string",
+							"required": true
+						}
+					}
+
+				},
+
+				{
+
+					"name": "U",
+
+
+					"fields": {
+
+						"F": {
+							"type": "string",
+							"required": true
+						},
+
+						"G": {
+							"type": "float"
+						},
+
+						"H": {
+							"type": "string"
+						}
+					}
+				}
+
+			]
+		);
+		expect(v).to.deep.equal(
+			{ 
+				valid: true,
+  			    warnings: [] 
+  			}
+  		);
+		done();
+	});
 });
