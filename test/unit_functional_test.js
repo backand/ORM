47aba6355112e91999cf88d8f5905012a4ff25bf
Yoram Kornatzky <kornatzky@gmail.com>
2015-07-22 08:36:30 +0300

    Completed Unit Tests for Validate

--- test/unit_functional_test.js
+++ test/unit_functional_test.js
@@ -167,4 +167,191 @@ describe("validate", function(){
   		);
 		done();
 	});
+
+	it("tables with n:m relationships are not allowed", function(done){
+		var v = validator(
+			[ 	
+							
+				{ 
+					name: "R", 
+					fields: {
+						a: {
+							type: "float"
+						},
+						b: {
+							type: "string"
+						},
+						dogs: {
+							collection: "U",
+							via: "owner"
+						}
+					}
+				},
+				{ 
+					name: "S", 
+					fields: {
+						h: {
+							type: "float"
+						},
+						j: {
+							type: "string"
+						},
+						people: {
+							collection: "T",
+							"via": "myS"
+						}
+					}
+				},
+				{ 
+					name: "T", 
+					fields: {
+						z: {
+							type: "float"
+						},
+						q: {
+							type: "string"
+						},
+						myS: {
+							collection: "S",
+							"via": "people"
+						}
+					}
+				},
+
+				{ 
+					name: "U", 
+					fields: {
+						c: {
+							type: "float"
+						},
+						d: {
+							type: "string"
+						},
+						owner: {
+							object: 'R'
+						}
+					}
+				}
+
+			]
+		);
+		expect(v).to.deep.equal(
+			{ 
+				valid: false,
+  			    warnings: [
+  			    	"multi select relationship are not allowed: relation S attribute people and relation T attribute myS",
+      			    "multi select relationship are not allowed: relation T attribute myS and relation S attribute people"
+  			    ] 
+  			}
+  		);
+		done();
+	});
+
+	it("defalt values of columns are of the right type", function(done){
+		var v = validator(
+			[ 	
+							
+			 	{
+					name: "user",
+					fields: {
+						name: {
+							type: 'string',
+							defaultValue: 200
+						},
+						age: {
+							type: 'datetime',
+							defaultValue: '2015-09-08'
+						},
+						dogs:{
+							collection: 'pet',
+							via: 'owner'
+						}
+					}
+				},	
+
+				{ 
+
+					name: "pet",
+
+					fields: {
+						name: {
+							type: 'string'
+						},
+						registered: {
+							type: 'boolean'
+						},
+						owner:{
+							object: 'user'
+						}
+					}
+					
+				}
+
+			]
+		);
+		expect(v).to.deep.equal(
+			{ 
+				valid: false,
+  			    warnings: ["column default value should be a string:user name"] 
+  			}
+  		);
+		done();
+	});
+
+	it("multiple 1:n relationships between two relations", function(done){
+		var v = validator(
+			[ 	
+							
+				{
+					name: "person",
+
+					fields: {
+							name: {
+							type: 'string'
+						},
+						age: {
+							type: 'datetime'
+						},
+						dogs:{
+							collection: 'animal',
+							via: 'catOwner'
+						},
+						cats:{
+							collection: 'animal',
+							via: 'dogOwner'
+						}
+					}
+				},
+
+				{
+					name: "animal",
+
+					fields: {
+						name: {
+							type: 'string'
+						},
+						registered: {
+							type: 'boolean'
+						},
+						dogOwner:{
+							object: 'person'
+						},
+						catOwner:{
+							object: 'person'
+						}
+					}
+				
+				}
+
+
+			]
+		);
+		expect(v).to.deep.equal(
+			{ 
+				valid: true,
+  			    warnings: [] 
+  			}
+  		);
+		done();
+	});
 });
