536accaa0a5ab66ad6a62f505750c47670fd172d
kornatzky <kornatzky@gmail.com>
2015-10-29 09:35:08 +0200

    Push
    send push to multiple devices

--- push/gcm_sender.js
+++ push/gcm_sender.js
@@ -1,13 +1,10 @@
 var gcm = require('node-gcm');
+var _ = require('underscore');
 
 
+var sender = new gcm.Sender('AIzaSyCY8AOb3qF4dVODMkBELjR7n1ClfFnBwq4');
 
-var sender = new gcm.Sender('AIzaSyDmYs5TmhVT_l4omMBZdWm_2n5uFQ4gpFw');
-  
-
-
-
-function   sendMessage(deviceId, type, value, callback) {
+function sendMessage(deviceIds, messageLabel, msgObject, callback) {
 
 
     var message = new gcm.Message({
@@ -17,40 +14,19 @@ function   sendMessage(deviceId, type, value, callback) {
         dryRun: false
     });
 
-    var registrationIds = [];
-    var messageLabel = "";
-    switch(type)
-    {
-
-        case "order": 
-            messageLabel = "התקבלה הזמנה חדשה";
-        break;
-
-        case "response":
-            messageLabel = "התקבלה תגובה להזמנה";
-        break;
-
-        default:
-        break;
-    }
-
-
-    message.addDataWithObject({
-        key1: type,
-        key2: value,
-        message: messageLabel,
-        msgcnt: 1
-    });
-
-
-    // At least one required
-    registrationIds.push(deviceId);
-
+    message.addData(
+        _.extend(msgObject, 
+            {
+                message: messageLabel,
+                msgcnt: 1
+            }
+        )
+    );
 
     /**
      * Params: message-literal, registrationIds-array, No. of retries, callback-function
      **/
-    sender.send(message, registrationIds, 4, function (err, result) {
+    sender.send(message, deviceIds, 4, function (err, result) {
         console.log(err);
         console.log(result);
         callback(err || result.success != 1 && result.failure == 0);
@@ -58,10 +34,10 @@ function   sendMessage(deviceId, type, value, callback) {
 
 }
 
-var deviceId = "APA91bG6VGJUxvZTmbGhOm7RUTMwwKbtyi0MC_utrZR3q4WukGtz2PzNyILXmTxTeN_vpPBmSQnvGBKLZtTr6zBSll0Qad0I7jtnwf1U1-hOQB--W7nDMvKuqjfAhSfettuKN1HI5d29";
+// var deviceId = "APA91bG6VGJUxvZTmbGhOm7RUTMwwKbtyi0MC_utrZR3q4WukGtz2PzNyILXmTxTeN_vpPBmSQnvGBKLZtTr6zBSll0Qad0I7jtnwf1U1-hOQB--W7nDMvKuqjfAhSfettuKN1HI5d29";
 
 
-sendMessage(deviceId, "product", 5, function(err){
-    console.log(err);
-    process.exit(1);
-});
+// sendMessage(deviceId, "A new product was added", { "product": "Samsung TV", "productId": 100 }, function(err){
+//     console.log(err);
+//     process.exit(1);
+// });
