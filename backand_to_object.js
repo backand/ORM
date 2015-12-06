93c4caae0e5075e3f9123cb06ef9bed6e22a977c
kornatzky <kornatzky@gmail.com>
2015-12-06 09:07:37 +0200

    Backand to Object
    merged files

--- backand_to_object.js
+++ backand_to_object.js
@@ -13,7 +13,6 @@ var columnsUrl = api_url + "/1/table/config/";
 var getUserUrl = api_url + "/api/account/profile";
 
 var backandToJsonType = {
-<<<<<<< HEAD
 	"Numeric": "float",
 	"ShortText": "string",
 	"LongText": "text",
@@ -23,16 +22,6 @@ var backandToJsonType = {
 	"SingleSelect": "SingleSelect",
 	"MultiSelect": "MultiSelect",
 	"Point": "point"
-=======
-    "Numeric": "float",
-    "ShortText": "string",
-    "LongText": "text",
-    "Boolean": "boolean",
-    "Binary": "binary",
-    "DateTime": "datetime",
-    "SingleSelect": "SingleSelect",
-    "MultiSelect": "MultiSelect"
->>>>>>> origin/master
 };
 
 function getUserDetails(accessToken, anonymousToken, appName, callback) {
@@ -81,7 +70,7 @@ function getUserDetails(accessToken, anonymousToken, appName, callback) {
     );
 }
 
-<<<<<<< HEAD
+
 function fetchColumns(accessToken, tokenType, appName, tableName, dbName, withDbName, withIdColumn, callbackColumns){
 
 	request(
@@ -161,89 +150,7 @@ function fetchColumns(accessToken, tokenType, appName, tableName, dbName, withDb
 		}
 
 	);
-=======
-function fetchColumns(accessToken, tokenType, appName, tableName, dbName, withDbName, withIdColumn, callbackColumns) {
 
-    request(
-        {
-            url: columnsUrl + tableName,
-
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': tokenType + " " + accessToken,
-                'AppName': appName
-            },
-
-            method: 'GET'
-        },
-
-        function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                var body = JSON.parse(body);
-                async.map(body.fields,
-                    function (item, callback) {
-                        if (!withIdColumn && (item.name == "id" || item.name == "Id")) {
-                            callback(null, null);
-                        }
-                        else if (item.name == "createAt" || item.name == "updatedAt") {
-                            callback(null, null);
-                        }
-                        else {
-                            var description = {"name": item.name, "type": backandToJsonType[item.type]};
-                            if (withDbName && item.databaseName) {
-                                description["dbName"] = item.databaseName;
-                            }
-                            if (_.has(item, "relatedViewName") && item.relatedViewName)
-                                description.relatedViewName = item.relatedViewName;
-                            if (_.has(item, "relatedParentFieldName") && item.relatedParentFieldName)
-                                description.relatedParentFieldName = item.relatedParentFieldName;
-
-                            if (item.advancedLayout.required)
-                                description.required = true;
-                            // if (_.has(item, "minValue"))
-                            // 	description.minValue = item.minValue;
-                            // if (_.has(item, "maxValue"))
-                            // 	description.maxValue = item.maxValue;
-                            if (_.has(item.advancedLayout, "defaultValue") && item.advancedLayout.defaultValue != "")
-                                description.defaultValue = item.advancedLayout.defaultValue;
-                            if (item.type == "SingleSelect") {
-                                var collection = description.relatedViewName;
-                                description = _.extend(_.omit(description, "type", "relatedViewName", "relatedParentFieldName"), {"object": collection});
-                            }
-                            if (item.type == "MultiSelect") {
-                                var collection = description.relatedViewName;
-                                var via = description.relatedParentFieldName;
-                                description = _.extend(_.omit(description, "type", "relatedViewName", "relatedParentFieldName"), {
-                                    "collection": collection,
-                                    "via": via
-                                });
-                            }
-                            callback(null, description);
-                        }
-                    },
-                    function (err, results) {
-                        var results = _.filter(results, function (r) {
-                            return r;
-                        });
-                        var fields = _.object(_.pluck(results, "name"), _.map(results, function (c) {
-                            return _.omit(c, "name");
-                        }));
-                        var columnsDescription = {"name": tableName, "fields": fields};
-                        if (withDbName) {
-                            columnsDescription["dbName"] = dbName;
-                        }
-                        callbackColumns(null, columnsDescription);
-                    }
-                );
-
-            }
-            else {
-                callbackColumns(error ? error : response.statusCode, null);
-            }
-        }
-    );
->>>>>>> origin/master
 
 }
 
