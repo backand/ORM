d1a4877df9cbf837b2e5e79fccf0466bfa1c630c
relly <relly@backand.com>
2016-02-07 17:50:08 +0200

    fix issues

--- parse-migration/migrator.js
+++ parse-migration/migrator.js
@@ -24,30 +24,33 @@ Migrator.prototype = (function () {
     function insertClass(streamer, datalink, fileName, converter, className, bulkRunner, callback) {
         logger.info('start insert class ' + className);
         streamer.getData(datalink, fileName, function (fileName, data) {
+            logger.info('start getData ' + fileName);
 
             if (!data) {
                 callback();
             }
 
-
-            var sql = converter.getInsertStatement(className, function (error) {
-                // error report
-            })
-
-            var valuesForBulkInserts = [];
-            // read each Parse class and insert it into a table
-            for (var i in data) {
-                var json = data[i];
-                var valuesToInsert = converter.getValuesToInsertStatement(className, json, function (error) {
+            else {
+                var sql = converter.getInsertStatement(className, function (error) {
                     // error report
                 })
 
-                valuesForBulkInserts.push(valuesToInsert);
-            }
+                var valuesForBulkInserts = [];
 
-            bulkRunner.insert(sql, valuesForBulkInserts, function (error) {
-                // error report
-            }, callback);
+                // read each Parse class and insert it into a table
+                for (var i in data) {
+                    var json = data[i];
+                    var valuesToInsert = converter.getValuesToInsertStatement(className, json, function (error) {
+                        // error report
+                    })
+
+                    valuesForBulkInserts.push(valuesToInsert);
+                }
+
+                logger.info('bulkRunner insert ' + sql);
+                bulkRunner.insert(sql, valuesForBulkInserts, function (error) {
+                }, callback);
+            }
         });
     };
 
@@ -62,16 +65,18 @@ Migrator.prototype = (function () {
                 if (!updateStatements || updateStatements.length == 0) {
                     break; // class has no pointers so there is no point to run through its data
                 }
-                sql = sql + updateStatements.join(";");
+                sql = sql + updateStatements.join(";") + ";";
             }
 
             if (!sql){
                 callback();
             }
+            else {
+                bulkRunner.update(sql, function (error) {
+                    // error report
 
-            bulkRunner.update(sql, function (error) {
-                // error report
-            }, callback);
+                }, callback);
+            }
         });
     };
 
@@ -79,11 +84,12 @@ Migrator.prototype = (function () {
         var relations = parseSchema.getClassRelations(className, function (error) {
             // error report
         });
-        for (var i in relations) {
-            var relationName = relations[i];
+        //for (var i in relations) {
+        async.eachSeries(relations, function (rel, callback2) {
+            var relationName = rel;
             var fileName = "_Join_" + relationName + "_" + className;
-            updateRelation(streamer, datalink, fileName, converter, className, relationName, bulkRunner, callback);
-        }
+            updateRelation(streamer, datalink, fileName, converter, className, relationName, bulkRunner, callback2);
+        }, callback );
     };
 
     function updateRelation(streamer, datalink, fileName, converter, className, relationName, bulkRunner, callback) {
@@ -94,12 +100,17 @@ Migrator.prototype = (function () {
                 var updateStatements = converter.getUpdateStatementsForRelation(className, relationName, json, function (error) {
                     // error report
                 })
-                sql = sql + updateStatements.join(";");
+                sql = sql + updateStatements + ";";
             }
 
-            bulkRunner.update(sql, function (error) {
-                // error report
-            }, callback);
+            if (!sql){
+                callback();
+            }
+            else {
+                bulkRunner.update(sql, function (error) {
+                    // error report
+                }, callback);
+            }
         });
     };
 
@@ -111,46 +122,67 @@ Migrator.prototype = (function () {
         run: function (appName, accessToken, connectionInfo, datalink, schema) {
             // a schema wrapper with helping functions
             var parseSchema = new ParseSchema(schema);
+
             // converts json to SQL Insert commands and parameters
             var classJsonConverter = new ClassJsonConverter(parseSchema);
+
             // converts Parse Pointers to SQL Update commands
             var pointerConverter = new PointerConverter(parseSchema);
+
             // converts Parse Relations to SQL Update commands
             var relationConverter = new RelationConverter(parseSchema);
+
             // run MySQL bulk SQL commands
             var bulkRunner = new BulkRunner(connectionInfo);
+
             // read large json files
             var streamer = new Streamer();
 
             // insert data of all classes without Relations and Pointers
             async.series([function (callback) {
+                logger.info('start insertClass');
                 // iterate on each class in the schema
                 async.eachSeries(schema, function (sc, callback2) {
+
                     // get the class
                     var className = sc.className;
-                    logger.info('enter to schema ' + className);
+
+
                     // get the file equivalent to this class
                     var fileName = className + ".json";
+
                     // insert the file to equivalent MySQL table
-                    insertClass(streamer, datalink, fileName, classJsonConverter, className, bulkRunner, callback2);
-                }, callback);
+                    insertClass(streamer, datalink, fileName,  classJsonConverter, className, bulkRunner, callback2);
+                }, function() {
+                        logger.info('finish step insertClass');
+                    callback()
+                }
+                );
             },
                 function (callback) {
+                    logger.info('start Pointer step');
                     // update data of all classes Pointers
                     async.eachSeries(schema, function (sc, callback2) {
+
                         var className = sc.className;
                         var fileName = className + ".json";
                         updatePointers(streamer, datalink, fileName, pointerConverter, className, bulkRunner, callback2);
-                    },callback);
+                    },function() {
+                        logger.info('finish step pointer');
+                        callback()
+                    });
                 },
                 function (callback) {
-
+                    logger.info('start updateRelations step');
 
                     // update data of all classes Relations
                     async.eachSeries(schema, function (sc, callback2) {
                         var className = sc.className;
                         updateRelations(streamer, datalink, relationConverter, className, parseSchema, bulkRunner, callback2);
-                    }, callback);
+                    }, function() {
+                        logger.info('finish step updateRelations');
+                        callback()
+                    });
                 }]);
         }
     };
@@ -160,6 +192,7 @@ function test() {
 
     var cleaner = new Cleaner(testConnection);
     var migrator = new Migrator();
+    // perform database cleanup to initiate all the tables. only needed in the test
     cleaner.clean(testSchema, function (error) {
         console.log(error);
     }, function(){
