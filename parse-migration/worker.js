bbf5a247b877335755356f78c0e15b449daa1341
relly <relly@backand.com>
2016-02-22 15:01:16 +0200

    reset the process

--- parse-migration/worker.js
+++ parse-migration/worker.js
@@ -3,6 +3,13 @@
  */
 process.chdir(__dirname);
 
+var fs = require("fs");
+
+fs.watchFile(__filename, function(curr,prev) {
+    logger.info("close process for update");
+    process.exit();
+});
+
 
 var Worker = require('./workerInner').Worker;
 var worker = new Worker();
