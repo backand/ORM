61de340c464191c2e287e1122fe9a13afc738446
relly <relly@backand.com>
2016-04-14 17:22:37 +0300

    Add lambda integration to ORM projectt

--- lambda/delete_lambda_function.js
+++ lambda/delete_lambda_function.js
@@ -6,11 +6,11 @@ var lambda = new AWS.Lambda();
 function deleteLambdaFunctionFromS3(folder, functionName, callback){
 
   var params = {
-    FunctionName: folder + '_' + functionName, /* required */
+    FunctionName: functionName, /* required */
     // Qualifier: 'STRING_VALUE'
   };
   lambda.deleteFunction(params, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
+    if (err) console.error(err, err.stack); // an error occurred
     else     console.log(data);           // successful response
     callback(err, data);
   });
--- lambda/update_lambda_function.js
+++ lambda/update_lambda_function.js
@@ -1,24 +1,37 @@
 var AWS = require('aws-sdk')
+var zipS3Folder = require('./zip-s3-folder').zipS3Folder;
 AWS.config.loadFromPath('./hosting/aws-credentials.json');
 AWS.config.update({ 'region': 'us-east-1' });
 var lambda = new AWS.Lambda();
 
 function updateLambdaFunctionFromS3(bucket, folder, fileName, functionName, callback){
+  console.log("updateLambdaFunctionFromS3 started");
+  console.log("zipS3Folder started");
 
-  var params = {
-    FunctionName: folder + '_' + functionName, /* required */
-    Publish: true,
-    S3Bucket: bucket,
-    S3Key: folder + '/' + fileName,
-    // S3ObjectVersion: 'STRING_VALUE',
-    // ZipFile: new Buffer('...') || 'STRING_VALUE'
-  };
-  lambda.updateFunctionCode(params, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else     console.log(data);           // successful response
-    callback(err, data);
+  zipS3Folder(bucket, folder, fileName, function(err, data){
+    if (err){
+      console.error(err);
+      callback(err, null);
+    }
+    else{
+      
+      var params = {
+        FunctionName: functionName, /* required */
+        Publish: true,
+        S3Bucket: bucket,
+        S3Key: folder + '/' + fileName,
+        // S3ObjectVersion: 'STRING_VALUE',
+        // ZipFile: new Buffer('...') || 'STRING_VALUE'
+      };
+      lambda.updateFunctionCode(params, function(err, data) {
+        if (err) console.error(err, err.stack); // an error occurred
+        else     console.log(data);           // successful response
+        console.log("updateLambdaFunctionFromS3 ended");
+        callback(err, data);
+      });
+    }
+
   });
-
 }
 
 module.exports.updateLambdaFunctionFromS3 = updateLambdaFunctionFromS3;
