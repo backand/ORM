044de0f9f853366d5204125fcd1b6eed36560216
yariv@backand.com <yariv@backand.com>
2016-06-28 10:30:49 +0300

    remove traling zeros - move to unix epoch

--- logger-reply/utils/timeUtil.js
+++ logger-reply/utils/timeUtil.js
@@ -9,10 +9,16 @@ var fromStringToEpoch = function (timeTxt) {
         return null;
     }
 
-    return moment(timeTxt +'Z', 'MM/DD/YYYY h:mm:ss aZ').valueOf();
+    return moment(timeTxt +'Z', 'MM/DD/YYYY h:mm:ss aZ').unix();
 
 
 };
 
 
 module.exports = fromStringToEpoch;
+/*
+var t='3/24/2016 10:40:35 AM';
+var s=fromStringToEpoch(t)
+console.log(s)
+*/
+
