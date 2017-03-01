205a803842f711a4bd323fa6b764d5aea85e422c
Yoram Kornatzky <kornatzky@gmail.com>
2017-03-01 11:42:01 +0200

    Structured Logging from Node.js

--- logging/log_with_redis.js
+++ logging/log_with_redis.js
@@ -1,3 +1,7 @@
+var logEntry = 'log_api';
+
+var moment = require('moment');
+
 var RedisDataSource = require('../logger-reply/sources/RedisDataSource');
 
 function NodejsLogger(source) {
@@ -6,7 +10,34 @@ function NodejsLogger(source) {
 }
 
 NodejsLogger.prototype.log = function (msg){
-    this.redisDataSource.insertEvent(this.source, msg, function(err, data){});
+    this.redisDataSource.insertEvent(msg, function(err, data){});
+}
+
+NodejsLogger.prototype.logFields = function (req,
+	typeOfMessage, 
+	nodeModule, 
+	action, 
+	message, 
+	trace,
+){
+	var msg = {
+		Source: "NodeJS",
+		ID: req.ID,
+		LogType: (typeOfMessage == "exception" ? "1" : "3"), 
+		ExceptionMessage: (typeOfMessage == "exception" ? message : ""),
+		LogMessage: (typeOfMessage != "exception" ? message : ""),	
+		ApplicationName: this.source, 
+		Username: req.headers.username, 
+		MachineName: require("os").hostname(), 
+		Time: moment.utc().format(), 
+		Controller: nodeModule, 
+		Action: req.method,  
+		MethodName: action, 
+		Trace: trace, 
+		FreeText: req.headers.host + req.url.path, 
+		Guid: req.headers.Guid, 
+	};
+    this.redisDataSource.insertEvent(logEntry, msg, function(err, data){});
 }
 
 module.exports = NodejsLogger;
