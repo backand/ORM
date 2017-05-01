2033c702d35f22e521903a90e0c468d94d6c9062
Yoram Kornatzky <kornatzky@gmail.com>
2017-05-01 13:10:35 +0300

    Test Cloud Watch Works with Wait

--- test/test_cloudwatch.js
+++ test/test_cloudwatch.js
@@ -11,7 +11,8 @@ var jsonfile = require('jsonfile');
 var replace = require('replace-in-file');
 var async = require('async');
 var isWin = require('os').platform() === 'win32';
-var filterLambdaLogs = require(path + 'list-s3/filter_cloudwatch_logs').filterCloudwatchLogs;
+var waitLogs = require(path + 'list-s3/wait_for_cloudwatch_logs').waitLogs;
+var invokeLambdaAndLog = require(path + 'lambda/invoke_lambda_and_log').invokeLambdaAndLog;
 
 var apiUrl = "https://api.backand.com";
 
@@ -19,7 +20,13 @@ describe("lambda log", function(done){
 
 
   var lambdaUrl = null;
+  var payload = null;
   var lambdaConsoleMessage = null;
+  var startTime = null;
+  var endTime = null;
+  var requestId = null;
+  var file = '../hosting/aws-credentials.json';
+  var credentials = jsonfile.readFileSync(file);
 
   before(function(done){
     this.timeout(64000);
@@ -40,9 +47,6 @@ describe("lambda log", function(done){
           commandActionInit = commandActionInit.replace(/node_modules\/backand\/bin\/backand/g, 'node node_modules\\backand\\bin\\backand');
       }
     exec(commandActionInit, function(err, stdout, stderr) {
-      console.log(err);
-      console.log(stdout);
-      console.log(stderr);
       var lines = stdout.split('\n');
       lambdaUrl = _.find(stdout.split('\n'), function(s) { return _.startsWith(s, 'The action was deployed and can be tested at '); }).replace(/The action was deployed and can be tested at /, '');
       // test files exist
@@ -54,39 +58,6 @@ describe("lambda log", function(done){
         done();
       });
 
-    });
-  });
-
-  it("lambda deploy", function(done){
-    this.timeout(64000);
-    var r = Date.now();
-    lambdaConsoleMessage = '{"message":"Hello ' + r + '!"}';
-    var options = {
-      files: 'items/testrunlambda/index.js',
-      from: /var helloWorld = \{"message": "Hello World!"\};/g,
-      to: 'var helloWorld = {"message": "Hello ' + r + '!"}; console.log(helloWorld);',
-    };
-    replace.sync(options);
-    var commandActionDeploy = 'node_modules/backand/bin/backand action deploy --object items --action testrunlambda --master b83f5c3d-3ed8-417b-817f-708eeaf6a945 --user 9cf80730-1ab6-11e7-8124-06bcf2b21c8c  --app cli --folder items/testrunlambda';
-      if (isWin){
-          commandActionDeploy = commandActionDeploy.replace(/node_modules\/backand\/bin\/backand/g, 'node node_modules\\backand\\bin\\backand');
-      }
-    exec(commandActionDeploy, function(err, stdout, stderr) {
-      request.get(apiUrl + '/1/objects/action/items/?name=testrunlambda&parameters={}',
-          {
-            auth: {
-              'user': 'b83f5c3d-3ed8-417b-817f-708eeaf6a945',
-              'pass': '9cf80730-1ab6-11e7-8124-06bcf2b21c8c'
-            }
-          },
-          function(err, response, body){
-            expect(err).to.be.null;
-            var bodyObj = JSON.parse(body);
-            expect(bodyObj.StatusCode).to.be.equal(200);
-            expect(bodyObj.Payload).to.be.equal(lambdaConsoleMessage);
-            done();
-          }
-      );
     });
   });
 
@@ -103,30 +74,129 @@ describe("lambda log", function(done){
           expect(err).to.be.null;
           var bodyObj = JSON.parse(body);
           expect(bodyObj.StatusCode).to.be.equal(200);
-          expect(bodyObj.Payload).to.be.equal(lambdaConsoleMessage);
+          expect(JSON.parse(bodyObj.Payload)).to.be.deep.equal({"message":"Hello World!"});
           done();
         }
     );
 
   });
 
-  it("filter lambda log", function(done){
-    this.timeout(10 * 60 * 1000);
-    var file = '../hosting/aws-credentials.json';
-    var credentials = jsonfile.readFileSync(file);
-    var filterPattern = lambdaConsoleMessage.replace(/\{"message":"Hello /g, '').replace(/!"\}/g, '');
-    setTimeout(function(){
-      filterLambdaLogs('us-east-1', credentials.accessKeyId, credentials.secretAccessKey, '/aws/lambda/cli_items_testrunlambda', filterPattern, function(err, data){
+  it("lambda deploy", function(done){
+    this.timeout(64000);
+    var r = Date.now();
+    payload = '{"message":"Hello ' + r + '!"}';
+    lambdaConsoleMessage = r + ' ' + _.random(1.2, 5.2) + ' ' + r;
+    var options = {
+      files: 'items/testrunlambda/index.js',
+      from: /var helloWorld = \{"message": "Hello World!"\};/g,
+      to: 'var helloWorld = ' + payload + '; console.log("' + lambdaConsoleMessage + '");',
+    };
+    replace.sync(options);
+    var commandActionDeploy = 'node_modules/backand/bin/backand action deploy --object items --action testrunlambda --master b83f5c3d-3ed8-417b-817f-708eeaf6a945 --user 9cf80730-1ab6-11e7-8124-06bcf2b21c8c  --app cli --folder items/testrunlambda';
+      if (isWin){
+          commandActionDeploy = commandActionDeploy.replace(/node_modules\/backand\/bin\/backand/g, 'node node_modules\\backand\\bin\\backand');
+      }
+    exec(commandActionDeploy, function(err, stdout, stderr) {
+      request.get(apiUrl + '/1/objects/action/items/?name=testrunlambda&parameters={}',
+          {
+            auth: {
+              'user': 'b83f5c3d-3ed8-417b-817f-708eeaf6a945',
+              'pass': '9cf80730-1ab6-11e7-8124-06bcf2b21c8c'
+            }
+          },
+          function(err, response, body){
+            expect(err).to.be.null;
+            done();
+          }
+      );
+    });
+  });
+
+  it("modified lambda call", function(done){
+    this.timeout(64000);
+    request.get(apiUrl + '/1/objects/action/items/?name=testrunlambda&parameters={}',
+        {
+          auth: {
+            'user': 'b83f5c3d-3ed8-417b-817f-708eeaf6a945',
+            'pass': '9cf80730-1ab6-11e7-8124-06bcf2b21c8c'
+          }
+        },
+        function(err, response, body){
+          expect(err).to.be.null;
+          var bodyObj = JSON.parse(body);
+          expect(bodyObj.StatusCode).to.be.equal(200);
+          expect(JSON.parse(bodyObj.Payload)).to.be.deep.equal(JSON.parse(payload));
+          done();
+        }
+    );
+
+  });
+
+  // it"filter lambda log", function(done){
+  //   this.timeout(10 * 60 * 1000);
+  //   var file = '../hosting/aws-credentials.json';
+  //   var credentials = jsonfile.readFileSync(file);
+  //   var filterPattern = lambdaConsoleMessage;
+  //   setTimeout(function(){
+  //     filterLambdaLogs('us-east-1', credentials.accessKeyId, credentials.secretAccessKey, '/aws/lambda/cli_items_testrunlambda', filterPattern, 10000, function(err, data){
+  //       expect(err).to.be.null;
+  //       expect(data).to.not.be.null;
+  //       expect(data).to.not.be.empty;
+  //       var a = _.find(data, function(e){
+  //         return e.message.indexOf(filterPattern) > -1;
+  //       });
+  //       expect(a).to.not.be.undefined;
+  //       done();       
+  //     });      
+  //   }, 5 * 60 * 1000);
+  // });
+
+  it("invoke", function(done){
+    this.timeout(64000);
+    invokeLambdaAndLog(
+      'us-east-1', 
+      credentials.accessKeyId, 
+      credentials.secretAccessKey, 
+      "arn:aws:lambda:us-east-1:328923390206:function:cli_items_testrunlambda", 
+      {}, 
+      function(err, data){
         expect(err).to.be.null;
-        expect(data).to.not.be.null;
-        expect(data).to.not.be.empty;
-        var a = _.find(data, function(e){
-          return e.message.indexOf(filterPattern) > -1;
+        expect(data.logs).to.not.be.null;
+        expect(data.logs).to.not.be.empty;
+        expect(data.requestId).to.not.be.null;
+        expect(data.requestId).to.not.be.undefined;
+        requestId = data.requestId;
+        expect(data.startTime).to.not.be.null;
+        expect(data.startTime).to.not.be.undefined;
+        startTime = data.startTime;
+        expect(data.endTime).to.not.be.null;
+        expect(data.endTime).to.not.be.undefined;
+        endTime = data.endTime;
+        done(); 
+    });
+  });
+
+  it("waitLogs", function(done){
+    this.timeout(10 * 60 * 1000);
+    waitLogs(
+      'us-east-1', 
+      credentials.accessKeyId, 
+      credentials.secretAccessKey, 
+      '/aws/lambda/cli_items_testrunlambda', 
+      requestId,
+      10000,
+      startTime,
+      endTime, 
+      function(err, logs){
+        var a = _.find(logs, function(e){
+          return e.message.indexOf(lambdaConsoleMessage) > -1;
         });
+        console.log(a);
         expect(a).to.not.be.undefined;
-        done();       
-      });      
-    }, 5 * 60 * 1000);
+        done();
+      }
+    );
+
   });
 
   after(function(done){
